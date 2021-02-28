41e1f1138d76ad35a649ab32953c803f083a4128
JakubSyperek <162420@student.uwm.edu.pl>
2021-02-28 19:37:21 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+string = "La la, la la la la la na na na na na, La la na na, la la la la la na na na na na La la, la la la la la na na na na na, La la na na, la la la la la na na na na na"
+x = string.split()
+
+print(x)
